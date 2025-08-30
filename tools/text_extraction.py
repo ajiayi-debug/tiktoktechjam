1600e46498f4abe3698318b1590c8edec34b4878
Daven <thecluelessmc@gmail.com>
2025-08-31 04:14:14 +0800

    change

--- tools/text_extraction.py
+++ tools/text_extraction.py
@@ -138,12 +138,3 @@ class ExtractionText:
             "combined_text": combined,
         }
 
-if __name__ == "__main__":
-    result = ExtractionText(
-        username="kiwiiclaire",
-        video_path="video1.mp4",
-        description_path="description1.txt",
-        out_prefix="output1",
-        audio_filename="output1.wav",
-        overwrite=True,
-    ).run()
